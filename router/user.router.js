6bc613f0677608192dc671b83f406acb780dba76
shobhnath-Trigya <shobhnath@trigya.co>
2025-08-28 00:11:56 +0530

    verify jwt token

--- router/user.router.js
+++ router/user.router.js
@@ -2,8 +2,12 @@ import express from 'express';
 import {db} from '../db/index.js';
 import { usersTable } from '../models/index.js';
 import { eq } from 'drizzle-orm';
-import { randomBytes, createHmac } from 'crypto';
-import { signupPostRequestBodySchema } from '../validation/request.validation.js';
+import { signupPostRequestBodySchema,loginPostRequestBodySchema } from '../validation/request.validation.js';
+import { hashPasswordWithSalt } from '../utils/hash.js'
+import { getUserByEmail } from '../services/user.service.js';
+import jwt from 'jsonwebtoken';
+import { createUserToken } from '../utils/token.js';
+
 const router = express.Router();
 
 router.post('/signup', async (req, res) => {
@@ -16,12 +20,13 @@ router.post('/signup', async (req, res) => {
 
   const { firstname, lastname, email, password } = validationResult.data;
 
-  const [existingUser] =  await db.select({id:usersTable.id}).from(usersTable).where(eq(usersTable.email, email));
+  const existingUser =  await getUserByEmail(email);
 
   if(existingUser) return res.status(400).json({ message: `User with this email ${email} already exists` });
 
-  const salt =  randomBytes(256).toString('hex');
-  const hasPassword = createHmac('sha256', salt).update(password).digest('hex');
+
+  const {salt, password: hasPassword } = hashPasswordWithSalt(password);
+  
   const [user] = await db.insert(usersTable).values({
     firstname,
     lastname,
@@ -32,5 +37,26 @@ router.post('/signup', async (req, res) => {
 
   return res.status(201).json({ message: 'User created successfully', data:{userId:user.id} });
 });
+
+router.post('/login', async (req, res) => {
+  const validationResult = await loginPostRequestBodySchema.safeParseAsync(req.body);
+
+  if(validationResult.error){
+    return res.status(400).json({ message: 'Invalid request data', errors: validationResult.error });
+  }
+
+  const { email, password } = validationResult.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if(!existingUser) return res.status(400).json({ message: `User with this email ${email} does not exist` });
+  const { password: hashedPassword } = hashPasswordWithSalt(password, existingUser.salt);
+ if(existingUser.password !== hashedPassword) {
+  return res.status(401).json({ message: 'Invalid email or password' });
+ }
+
+const token = await createUserToken({ id: existingUser.id });
+ return res.status(200).json({ message: 'Login successful', data: { userId: existingUser.id, token } });
+})
 
 export default router;
--- validation/request.validation.js
+++ validation/request.validation.js
@@ -6,3 +6,8 @@ export const signupPostRequestBodySchema = z.object({
     email: z.string().email(),
     password: z.string().min(3),
 });
+
+export const loginPostRequestBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(3),
+})
